adf7e4e167c093d866fa78676fb3d38657170de7
sara alilach <saraalilach7@gmail.com>
2025-09-24 16:30:59 +0100

    code.js

---
+++ code.js
@@ -0,0 +1,6 @@
+let code =["C1","C3" ,"C4"];
+let coderecherche = "C1";
+let newcode = code. filter (C => C === coderecherche).length;
+
+console.log ( coderecherche + " apparait "  + newcode + " fois ");
+
